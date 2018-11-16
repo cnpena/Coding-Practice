928ca6402b2fe9538f9c3735231d0a7dca790f33
Celine Pena <celinenpena@gmail.com>
2018-11-16 14:06:19 -0800

    Basic linked list class

---
+++ Chapter 2/LinkedList.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+
+    def printl(self):
+        current  = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+    #For testing purposes
+    def asList(self):
+        current = self.head
+        list = []
+        while current:
+            list.append(current.data)
+            current = current.next
+        return list
+
+    def findNode(self,value):
+    current = self.head
+    while current:
+        if current.data == value:
+            return current
+        current = current.next
+    return None
