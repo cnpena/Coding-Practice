7356dcf4f9aa17758c6a6d1913e9e67512230b96
Celine Pena <celinenpena@gmail.com>
2018-11-19 19:31:09 -0800

    added some tests

--- 3.1_threeStacks.py
+++ 3.1_threeStacks.py
@@ -1,6 +1,8 @@
 #Chapter 3: Stacks and Queues
 #3.1 Three in One, page 98
 #Uses a single array to implement 3 stacks
+import unittest
+
 class FixedMultiStack:
 
 	def __init__(self, stackSize):
@@ -50,16 +52,73 @@ class FixedMultiStack:
 	#Returns index of the top of the stack
 	def topIndex(self, stackNum):
 		offset = stackNum * self.stackSize
-		return offset + self.sizes[stackNum] - 1
+		size = self.sizes[stackNum]
+		return offset + size - 1
+
+class Test(unittest.TestCase):
+
+	#Pushes a value to each stack
+	def pushToEachStack(self, stack, value):
+		stack.push(0, value)
+		stack.push(1, value)
+		stack.push(2, value)
+
+	def test_empty_stack_when_created(self):
+		#Stacks should be empty when created
+		stack = FixedMultiStack(4)
+		self.assertTrue(stack.isEmpty(0))
+		self.assertTrue(stack.isEmpty(1))
+		self.assertTrue(stack.isEmpty(2))
+
+	#Push value to each stack, test peek function
+	def test_peek_with_1_item(self):
+		stack = FixedMultiStack(4)
+		for value in range(3):
+			self.pushToEachStack(stack, value)
+			self.assertEqual(stack.peek(0), value)
+			self.assertEqual(stack.peek(1), value)
+			self.assertEqual(stack.peek(2), value)
+
+	#Push value to each stack, stacks should no longer be empty
+	def test_non_empty_stack_after_push(self):
+		stack = FixedMultiStack(4)
+		for value in range(3):
+			self.pushToEachStack(stack, value)
+			self.assertFalse(stack.isEmpty(0))
+			self.assertFalse(stack.isEmpty(1))
+			self.assertFalse(stack.isEmpty(2))
+
+	#Push values to each stack, test stack is not full
+	def test_stack_is_not_full(self):
+		stack = FixedMultiStack(4)
+		for value in range(3):
+			self.pushToEachStack(stack, value)
+			self.assertFalse(stack.isFull(0))
+			self.assertFalse(stack.isFull(1))
+			self.assertFalse(stack.isFull(2))
+
+	#Push values to each stack, test stack is full
+	def test_stack_is_full(self):
+		stack = FixedMultiStack(4)
+		for value in range(4):
+			self.pushToEachStack(stack, value)
+		self.assertTrue(stack.isFull(0))
+		self.assertTrue(stack.isFull(1))
+		self.assertTrue(stack.isFull(2))
+		
+		with self.assertRaises(Exception) as context:
+			stack.push(0, 1)
+		self.assertTrue('Stack is full' in str(context.exception))
 
 
-newstack = FixedMultiStack(2)
-print(newstack.isEmpty(1))
-newstack.push(1,3)
-print (newstack.peek(1))
-print (newstack.isEmpty(1))
-newstack.push(1,2)
-print (newstack.peek(1))
-print (newstack.pop(1))
-print (newstack.peek(1))
-newstack.push(1,3)
+		
+
+if __name__ == "__main__":
+	unittest.main()
+
+
+# newstack.push(1,2)
+# print (newstack.peek(1))
+# print (newstack.pop(1))
+# print (newstack.peek(1))
+# newstack.push(1,3)
