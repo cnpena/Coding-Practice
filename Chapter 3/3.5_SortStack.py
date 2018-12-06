b1dd50dd93cec193c444d408fee5976fa136557b
Celine Pena <cnpena@ucsc.edu>
2018-12-05 23:21:46 -0800

    added sort function

--- Chapter 3/3.5_SortStack.py
+++ Chapter 3/3.5_SortStack.py
@@ -4,6 +4,7 @@
 #You can use an additional temporary stack, but you may 
 #not copy the elements into any other data structure (such as an array).
 #The stack supports the following operations: push, pop, peek, and isEmpty.
+#Run time: O(n^2), Space: O(n)
 
 class SortStack:
 	def __init__(self):
@@ -20,3 +21,16 @@ class SortStack:
 
 	def peek(self):
 		return self.items[len(self.items)-1]
+
+	def sort(self):
+		stack2 = SortStack()
+		while not self.isEmpty():
+			#insert each element in self in sorted order into stack2
+			temp = self.pop()
+			while not stack2.isEmpty and stack2.peek() > temp:
+				self.push(stack2.pop())
+			stack2.push(temp)
+
+		#copy elements from stack2 back into self
+		while not stack2.isEmpty():
+			self.push(stack2.pop())
