fd8ba845ab2f52e07071ee022c3dca9102806544
Celine Pena <cnpena@ucsc.edu>
2019-01-02 23:38:48 -0800

    fixed some logic

--- Hackerrank/minimum_swaps.py
+++ Hackerrank/minimum_swaps.py
@@ -5,6 +5,15 @@
 import unittest
 
 def minimumSwaps(arr):
+	swaps = 0
+	i = 0
+	while i<len(arr):
+		if arr[i]==(i+1):
+			i+=1
+			continue
+		arr[arr[i]-1], arr[i] = arr[i], arr[arr[i]-1]
+		swaps+=1
+	return swaps
 
 class Test(unittest.TestCase):
 	def test_sorted_list(self):
@@ -17,10 +26,10 @@ class Test(unittest.TestCase):
 		output = minimumSwaps(input)
 		self.assertEqual(output, 1)
 
-	def test_reverse_sorted(self)
+	def test_reverse_sorted(self):
 		input = [3, 2, 1]
 		output = minimumSwaps(input)
 		self.assertEqual(output, 1)
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
