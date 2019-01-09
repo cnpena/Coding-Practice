23a3684370bab5b5b1abe67020439b15051a0dab
Celine Pena <cnpena@ucsc.edu>
2019-01-08 22:13:30 -0800

    added tests

--- Hackerrank/newYearChaos.py
+++ Hackerrank/newYearChaos.py
@@ -11,14 +11,34 @@
 
 import unittest
 
+#Iterates through the array, first checking if it is possible to get this configuration
+#with every person only bribing at most two others. In order for this to be true, 
+#no person should be more than two places in front of where they started.
+#Otherwise if valid, counts the swaps that took place.
 def minimumBribes(q):
-    bribes = 0
-    for index, value in enumerate(q):
-        if (value - 1) - index > 2:
-            print('Too chaotic')
-            return
+	bribes = 0
+	for index, value in enumerate(q):
+		if (value - 1) - index > 2:
+			return 'Too chaotic'
 
-        for j in range(max(0, q[index] - 2), index):
-            if q[j] > q[index]:
-                bribes+=1
-    print(bribes)
+		for j in range(max(0, q[index] - 2), index):
+			if q[j] > q[index]:
+				bribes+=1
+	return bribes
+
+class Test(unittest.TestCase):
+	validLines = [[2, 1, 5, 3, 4], [1, 2, 5, 3, 7, 8, 6, 4]]
+	validLinesSol = [3, 7]
+
+	chaoticLines = [[2, 5, 1, 3, 4], [5, 1, 2, 3, 7, 8, 6, 4]]
+
+	def test_valid_lines(self):
+		for i in range(len(self.validLines)):
+			self.assertEquals(minimumBribes(self.validLines[i]), self.validLinesSol[i])
+
+	def test_chaotic_lines(self):
+		for i in range(len(self.chaoticLines)):
+			self.assertEquals(minimumBribes(self.chaoticLines[i]), 'Too chaotic')
+
+if __name__ == "__main__":
+	unittest.main()
