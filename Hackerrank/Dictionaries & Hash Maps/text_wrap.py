c792eb4e1662b71e848c22a7f4708e0699bdff5f
Celine Peña <cnpena@ucsc.edu>
2019-02-25 18:10:08 -0800

    added some comments

--- Hackerrank/Dictionaries & Hash Maps/text_wrap.py
+++ Hackerrank/Dictionaries & Hash Maps/text_wrap.py
@@ -4,14 +4,20 @@
 # are removed such that each character occurs only once within each substring
 # Constraints (specified in problem statement): Guaranteed that n is a multiple of k.
 
+# This solution works by iterating through the string. At each iteration,
+# we look at the k characters in the substring. A set is used to prevent 
+# duplicate values from being printed in each substring with O(1) lookup. 
+# If a character is not a duplicate, it is added to the output string which
+# is printed when the whole substring has been looked at.
 def merge_the_tools(string, k):
     while string:
-        s = string[0:k]
+        s = string[0:k] # Get substring of length k
         seen = set()
         output = ''
+
         for char in s:
             if char not in seen:
-                seen.add(char)
+                seen.add(char) # Add char to set and output string
                 output +=char
         print(output)
-        string = string[k:]
+        string = string[k:] # Move to next k characters
