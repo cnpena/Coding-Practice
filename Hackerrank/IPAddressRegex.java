a740f803ef85dd280236a46d0bbf3f499af759a8
Celine Peña <cnpena@ucsc.edu>
2019-03-03 23:17:13 -0800

    new java file

---
+++ Hackerrank/IPAddressRegex.java
@@ -0,0 +1,9 @@
+/* IP Address Regex
+	Class IPAddressRegex that contains a string pattern. Uses a regular expression to
+	validate an IP address based on the following definition:
+	
+		IP address is a string in the form "A.B.C.D", 
+			where the value of A, B, C, and D may range from 0 to 255. 
+		Leading zeros are allowed. 
+		The length of A, B, C, or D can't be greater than 3.
+*/
