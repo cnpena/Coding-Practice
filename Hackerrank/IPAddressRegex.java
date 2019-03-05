513f84d90c97bfdac50c9c728ccf1d227034ddd9
Celine Peña <cnpena@ucsc.edu>
2019-03-04 19:01:46 -0800

    cleaned up code, added comments

--- Hackerrank/IPAddressRegex.java
+++ Hackerrank/IPAddressRegex.java
@@ -1,5 +1,5 @@
 /* IP Address Regex
-	Class IPAddressRegex that contains a string pattern. Uses a regular expression to
+	Class IPAddressRegex uses a regular expression to
 	validate an IP address based on the following definition:
 
 		IP address is a string in the form "A.B.C.D", 
@@ -8,7 +8,20 @@
 		The length of A, B, C, or D can't be greater than 3.
 */
 
-class MyRegex {
-    String single = "([0-9]|[0-9][0-9]|[0-1][0-9][0-9]|2[0-4][0-9]|25[0-5])";
-    String pattern = single + "." + single + "." + single + "." + single;
+/* To match the regular expression to valid IP addresses, must consider
+	values of length 1, 2, and 3.
+		For values of length 1, can be any digit 0-9
+		For values of length 2, each can be any digit 0-9
+		For values of length 3, can be
+			000-199 OR
+			200-249 OR
+			250-255
+*/
+			
+class IPAddressRegex {
+	String oneDigit = "[0-9]";
+    String twoDigits = "[0-9][0-9]";
+    String threeDigits = "[0-1][0-9][0-9]|[2][0-4][0-9]|25[0-5]";
+    String exp = "(" + oneDigit + "|" + twoDigits + "|" + threeDigits + ")";
+    String pattern = exp + "." + exp + "." + exp + "." + exp;
 }
