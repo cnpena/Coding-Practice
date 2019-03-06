b11b015fa70552883182a1d00e068837ad2c901b
Celine Peña <cnpena@ucsc.edu>
2019-03-05 20:51:53 -0800

    added some testing code

--- Hackerrank/IPAddressRegex.java
+++ Hackerrank/IPAddressRegex.java
@@ -17,8 +17,24 @@
 			200-249 OR
 			250-255
 */
-			
+	
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
 class IPAddressRegex {
+
+    public static void main(String[] args){
+ 
+    	String[] tests = {"000.12.12.034", "121.234.12.12", "23.45.12.56", "00.12.123.123123.123", "122.23", "Hello.IP"};
+        
+        for(int i = 0; i < tests.length; i++){
+        	System.out.println(tests[i].matches(new IP().pattern));
+        }
+    }
+}
+
+class IP {
 	String oneDigit = "[0-9]";
     String twoDigits = "[0-9][0-9]";
     String threeDigits = "[0-1][0-9][0-9]|[2][0-4][0-9]|25[0-5]";
