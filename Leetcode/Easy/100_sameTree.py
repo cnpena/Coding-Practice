77ec7193bc002f87b370a7ece269bd8e7f322765
Celine Peña <cnpena@ucsc.edu>
2019-03-16 23:39:18 -0700

    added a few comments + reference link

--- Leetcode/Easy/100_sameTree.py
+++ Leetcode/Easy/100_sameTree.py
@@ -1,5 +1,10 @@
+# 100. Same Tree
+# Given two binary trees, checks if they are the same or not.
+# Contains no driver code but has been tested on Leetcode.
+
 # Checks if they are equivalent by performing a recursive traversal through both trees
 # O(n) time, O(logn) / O(n) space depending on how balanced the tree is.
+# Reference: https://www.geeksforgeeks.org/write-c-code-to-determine-if-two-trees-are-identical/
 class Solution(object):
     def isSameTree(self, p, q):
         if not p and not q: #if both null, continue
