f3507c247205516e020b09b68a2e9c8667f9518f
Celine Peña <cnpena@ucsc.edu>
2019-03-16 23:37:49 -0700

    changed and fixed, works now

--- Leetcode/Easy/100_sameTree.py
+++ Leetcode/Easy/100_sameTree.py
@@ -1,15 +1,15 @@
-# Checks if they are equivalent by performing an inOrderTraversal through both trees
-# If we come upon a node that is not consistent between trees, return False.
+# Checks if they are equivalent by performing a recursive traversal through both trees
+# O(n) time, O(logn) / O(n) space depending on how balanced the tree is.
 class Solution(object):
     def isSameTree(self, p, q):
-        return inOrderTraversal(p,q)
+        if not p and not q: #if both null, continue
+            return True
         
-    
-def inOrderTraversal(node1, node2):
-    if node1 and node2:
-        inOrderTraversal(node1.left, node2.left)
-        if (node1.val != node2.val):
+        if not p or not q: #if only one is null, cannot be same, return false
             return False
-        inOrderTraversal(node1.right, node2.right)
-    return True
+    
+        if p.val != q.val: #if they have different values, cannot be same, return false
+            return False
+        
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
         
