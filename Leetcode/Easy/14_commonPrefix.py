85a01f213de587c7d0efaec84600e5b242980b4e
Celine Peña <cnpena@ucsc.edu>
2019-03-28 23:03:31 -0700

    add file

---
+++ Leetcode/Easy/14_commonPrefix.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    # Works by iterating through the characters in the first string in the list.
+    # For each character, checks all other strings in the list to see if the
+    # character at the same index matches. Stops when it reaches the end of the
+    # string or one of the strings doesn't match. In this case, returns a string
+    # containing the letters that have been matched.
+    def longestCommonPrefix(self, strs):
+        if len(strs) == 0: return ""
+        
+        for i in range(len(strs[0])):
+            currentChar = strs[0][i]
+            for j in range(1, len(strs)):
+                if i == len(strs[j]) or currentChar != strs[j][i]:
+                    return strs[0][0:i]
+        return strs[0]
