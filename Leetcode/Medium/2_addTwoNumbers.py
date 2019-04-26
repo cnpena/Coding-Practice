5d7f353674ea3ca9478542efc93edfb4ba1c7d71
Celine Peña <cnpena@ucsc.edu>
2019-04-25 20:37:38 -0700

    one more comment

--- Leetcode/Medium/2_addTwoNumbers.py
+++ Leetcode/Medium/2_addTwoNumbers.py
@@ -3,6 +3,8 @@
 # The digits are stored in reverse order and each of their nodes contain a single digit. 
 # Add the two numbers and return it as a linked list.
 
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
 class Solution(object):
     # Works by iterating through both lists, adding the values. 
     # Computes and adds the carry value as we iterate. 
